012987adc6db6013f844c1825a46a6f2622d1fc3
pedroortegaortiz <pemioror@gmail.com>
2026-02-12 07:45:32 -0600

    pacman

--- java/src/lospedros/edu/pacman/ui/GamePanel.java
+++ java/src/lospedros/edu/pacman/ui/GamePanel.java
@@ -1,6 +1,8 @@
 package lospedros.edu.pacman.ui;
 
+import lospedros.edu.pacman.process.CollisionChecker;
 import lospedros.edu.pacman.process.Pacman;
+import lospedros.edu.pacman.tile.TileManager;
 import lospedros.edu.pacman.utils.GameLocale;
 
 import javax.swing.JPanel;
@@ -13,15 +15,21 @@ import java.awt.Font;
 public class GamePanel extends JPanel implements Runnable {
 
     final int originalTileSize = 16;
-    final int scale = 3;
-    public final int tileSize = originalTileSize * scale;
-    public final int maxScreenCol = 16;
-    public final int maxScreenRow = 12;
+    final int scale = 2; // Smaller scale to fit more tiles
+    public final int tileSize = originalTileSize * scale; // 32x32
+    public final int maxScreenCol = 21; // More columns
+    public final int maxScreenRow = 23; // More rows
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
 
     Thread gameThread;
     KeyHandler keyH = new KeyHandler();
+    
+    // System
+    public TileManager tileM = new TileManager(this);
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    
+    // Entities
     Pacman player = new Pacman(this, keyH);
     
     // Game State
@@ -64,14 +72,17 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         
-        // Draw Player
+        // Draw Tiles (Layer 0)
+        tileM.draw(g2);
+        
+        // Draw Player (Layer 1)
         player.draw(g2);
         
-        // Draw UI
+        // Draw UI (Layer 2)
         g2.setColor(Color.white);
         g2.setFont(new Font("Arial", Font.PLAIN, 20));
         g2.drawString(GameLocale.getString("game.score") + score, 10, 20);
-        g2.drawString(GameLocale.getString("game.lives") + lives, screenWidth - 100, 20);
+        g2.drawString(GameLocale.getString("game.lives") + lives, screenWidth - 150, 20);
 
         g2.dispose();
     }
